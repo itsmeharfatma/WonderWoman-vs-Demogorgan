caa25062bf22aca87dda5f54a29e9818de369877
Mehar <mehar.fatma202@gmail.com>
2023-10-03 22:19:02 +0530

    added js code for collision detection and added more css

--- script.js
+++ script.js
@@ -1,11 +1,64 @@
+score = 0;
+cross = true;
 
 document.addEventListener("keydown", (e) => {
     console.log(e.key);
     if (e.key === "ArrowUp") {
-        ww = document.querySelector(".ww");
-        ww.classList.add("animateWw");
+        wonderWoman = document.querySelector(".wonderWoman");
+        wonderWoman.classList.add("animateWonderWoman");
         setTimeout(() => {
-            ww.classList.remove("animateWw")
+            wonderWoman.classList.remove("animateWonderWoman")
         }, 700);
     }
+    if (e.key === "ArrowRight") {
+        wonderWoman = document.querySelector(".wonderWoman");
+        wonderWomanX = parseInt(window.getComputedStyle(wonderWoman, null).getPropertyValue("left"));
+        wonderWoman.style.left = (wonderWomanX + 100) + "px";
+    }
+    if (e.key === "ArrowLeft") {
+        wonderWoman = document.querySelector(".wonderWoman");
+        wonderWomanX = parseInt(window.getComputedStyle(wonderWoman, null).getPropertyValue("left"));
+        wonderWoman.style.left = (wonderWomanX - 100) + "px";
+    }
 });
+
+// colision detection
+setInterval(() => {
+    wonderWoman = document.querySelector(".wonderWoman");
+    gorgan = document.querySelector(".gorgan");
+    gameOver = document.querySelector(".gameOver");
+
+    wonderWomanX = parseInt(window.getComputedStyle(wonderWoman, null).getPropertyValue("left"));
+    wonderWomanY = parseInt(window.getComputedStyle(wonderWoman, null).getPropertyValue("bottom"));
+
+    gorganX = parseInt(window.getComputedStyle(gorgan, null).getPropertyValue("left"));
+    gorganY = parseInt(window.getComputedStyle(gorgan, null).getPropertyValue("bottom"));
+
+    offsetX = Math.abs(wonderWomanX - gorganX);
+    offsetY = Math.abs(wonderWomanY - gorganY);
+
+
+    if (offsetX < 75 && offsetY < 30) {
+        gameOver.style.visibility = "visible";
+        gorgan.classList.remove("animateGorgan")
+    }
+    else if (offsetX < 75 && cross) {
+        score += 1;
+        updatedScore(score);
+        cross = false;
+        setTimeout(() => {
+            cross = true;
+        }, 1000);
+
+        setTimeout(() => {
+            animationDuration = gorganX = parseFloat(window.getComputedStyle(gorgan, null).getPropertyValue("animation-duration"));
+            newDuration = animationDuration - 0.2;
+            gorgan.style.animationDuration = newDuration + "s";
+        }, 500);
+    }
+
+}, 10);
+
+function updatedScore(score) {
+    scoreCont.innerHTML = "Your Score: " + score
+}
